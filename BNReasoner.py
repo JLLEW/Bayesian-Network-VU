afb6e5386bc6b943faff8b0815ed8de285c786f6
jacob.lewkowicz <jacob.lewkowicz@gmail.com>
2022-12-10 14:07:25 +0100

    Implement pruning and d-separation

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,9 @@
 from typing import Union
 from BayesNet import BayesNet
+from copy import deepcopy
+from networkx.utils import UnionFind
+import pandas as pd
+import networkx as nx
 
 
 class BNReasoner:
@@ -15,4 +19,88 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # TODO: This is where your methods should go
+    # METHODS FOR ASSIGNMENT -------------------------------------------------------------------------------------
+
+    # network pruning
+    def prune(self, queries: list, evidence: dict) -> BayesNet:
+
+        output_net = deepcopy(self.bn)
+
+        # edge prunning
+        for var in evidence.keys():
+            # update conditional probability table based on evidence
+            cpt = output_net.get_cpt(var)
+            output_net.update_cpt(var, cpt[cpt[var] == evidence[var]])
+
+            children = output_net.get_children(var)
+            for child in children:
+                # remove the edge
+                output_net.del_edge((var, child))
+                # update the conditional probability table
+                cpt = output_net.get_cpt(child)
+                output_net.update_cpt(child, cpt[cpt[var] == evidence[var]])
+
+        # leaf nodes prunning
+        while True:
+            leaf_vars = [var for var in output_net.get_all_variables() if not output_net.get_children(var)]
+            
+            # if there are no more nodes to prune
+            if not leaf_vars - set(queries + list(evidence.keys())):
+                break
+            
+            for leaf in leaf_vars:
+                if leaf not in queries and leaf not in evidence:
+                    output_net.del_var(leaf)
+        
+        return output_net
+
+    # d-separation
+    def is_d_seperated(self, X: list, Y: list, Z: list) -> bool:
+        # make sure that X, Y and Z are sets and conists of unique variables
+        X = set(X)
+        Y = set(Y)
+        Z = set(Z)
+
+        graph = deepcopy(self.bn)
+        XYZ = X.union(Y).union(Z)
+
+        # remove leaf nodes that are not in union of X, Y, Z
+        while True:
+            leaf_vars = [var for var in graph.get_all_variables() if not graph.get_children(var)]
+            # if there are no more nodes to prune
+            if not set(leaf_vars) - XYZ:
+                break
+            
+            for leaf in leaf_vars:
+                if leaf not in XYZ:
+                    graph.del_var(leaf)
+        
+        # remove outgoing edges for each node from Z
+        edges = graph.structure.out_edges(Z)
+        for edge in edges:
+            graph.del_edge(edge)
+
+        # create empty UnionFind instance
+        disjoint_set = UnionFind(graph.structure.nodes())
+        # get weakly connected components
+        for wcc in nx.weakly_connected_components(graph.structure):
+            disjoint_set.union(*wcc)
+
+        disjoint_set.union(*X)
+        disjoint_set.union(*Y)
+
+        # early condition if any of sets is empty
+        if not X or not Y:
+            return False
+
+        # to check if X is not d-seperated from Y, we just have to check if any of vars from X is in Y
+        for x in X:
+            for y in Y:
+                if disjoint_set[y] == disjoint_set[x]:
+                    return False
+
+        return True
+
+    
+
+
