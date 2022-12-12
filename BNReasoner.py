fc4fcbf826d94d1a8490181a564037aa5c880c41
jacob.lewkowicz <jacob.lewkowicz@gmail.com>
2022-12-12 17:07:36 +0100

    Add ordering and variable elimination

--- BNReasoner.py
+++ BNReasoner.py
@@ -124,11 +124,12 @@ class BNReasoner:
         marginalized = factor.groupby(vars).sum().reset_index()
         if 'p' in marginalized.columns:
             marginalized = marginalized.drop(var, axis=1)
-            marginalized = marginalized.rename(columns={"p" : f"\u03A3 {var}"})
 
         return marginalized
 
     def factors_multiplication(self, factors: list) -> pd.DataFrame:
+        # remove None elements from factors list
+        factors = [factor for factor in factors if factor is not None]
         # construct empty output factor as a truth table
         vars = [x.columns.values.tolist() for x in factors]
         vars = set(itertools.chain.from_iterable(vars))
@@ -139,13 +140,64 @@ class BNReasoner:
 
         # update truth_table by a multiplication of p from compatible rows from factors
         for factor in factors:
+            vars = set(factor.columns.values.tolist())
+            vars.remove('p')
+            vars = list(vars)
             for index, row in truth_table.iterrows():
                 series = pd.Series(row)
-                vars = set(factor.columns.values.tolist())
-                vars.remove('p')
-                vars = list(vars)
                 series = series[vars]
                 p = self.bn.get_compatible_instantiations_table(series, factor).iloc[0]['p']
                 truth_table.at[index, 'p'] *=  p
 
         return truth_table
+
+    def min_degree_ordering(self, X: list) -> list:
+        graph = self.bn.get_interaction_graph()
+        degrees = [x for x in graph.degree() if x[0] in X]
+        degrees.sort(key=lambda x : x[1])
+        ordered = [x[0] for x in degrees]
+        return ordered
+
+    def min_fill_ordering(self, X: list):
+        graph = self.bn.get_interaction_graph()
+        neighbours = [(x, [n for n in graph.neighbors(x)]) for x in X]
+        edges = graph.edges
+        min_fill_degrees = []
+        for n in neighbours:
+            node_name = n[0]
+            score = 0
+            n_len = len(n[1])
+            for i in range(n_len):
+                for j in range(i, n_len):
+                    # if edge not in graph
+                    if not ((i, j) in edges or (j, i) in edges):
+                        score += 1
+            min_fill_degrees.append((node_name, score))
+
+        print(f"min fill degrees {min_fill_degrees}")
+        min_fill_degrees.sort(key=lambda x: x[1])
+        ordered = [x[0] for x in min_fill_degrees]
+            
+        return ordered
+
+    def variable_elimination(self, X: list, ordering="min_degree") -> pd.DataFrame:
+        if ordering == "min_degree":
+            X = self.min_degree_ordering(X)
+        else:
+            X = self.min_fill_ordering(X)
+
+        all_cpts = self.bn.get_all_cpts().values()
+        
+        prev_factor = None
+        for x in X:
+            x_cpts = [cpt for cpt in all_cpts if x in cpt.columns]
+            all_cpts = [cpt for cpt in all_cpts if x not in cpt.columns]
+            # add prev_factor
+            x_cpts += [prev_factor]
+            multiplied = self.factors_multiplication(x_cpts)
+            marginalized = self.marginalize(multiplied, x)
+            prev_factor = marginalized
+
+        # prev_factor after all iterations is a final marginalized factor
+        return prev_factor
+
