a0d93983432776681f8bbdd6fda77e3639c6a27f
jacob.lewkowicz <jacob.lewkowicz@gmail.com>
2022-12-13 15:46:42 +0100

    Implement MAP, MPE and add tests

--- BNReasoner.py
+++ BNReasoner.py
@@ -20,18 +20,16 @@ class BNReasoner:
         else:
             self.bn = net
 
-    # METHODS FOR ASSIGNMENT -------------------------------------------------------------------------------------
-
     # network pruning
-    def prune(self, queries: list, evidence: dict) -> BayesNet:
+    def prune(self, Q: list, E: dict) -> BayesNet:
 
         output_net = deepcopy(self.bn)
 
         # edge prunning
-        for var in evidence.keys():
+        for var in E.keys():
             # update conditional probability table based on evidence
             cpt = output_net.get_cpt(var)
-            output_net.update_cpt(var, cpt[cpt[var] == evidence[var]])
+            output_net.update_cpt(var, cpt[cpt[var] == E[var]])
 
             children = output_net.get_children(var)
             for child in children:
@@ -39,18 +37,18 @@ class BNReasoner:
                 output_net.del_edge((var, child))
                 # update the conditional probability table
                 cpt = output_net.get_cpt(child)
-                output_net.update_cpt(child, cpt[cpt[var] == evidence[var]])
+                output_net.update_cpt(child, cpt[cpt[var] == E[var]])
 
         # leaf nodes prunning
         while True:
             leaf_vars = [var for var in output_net.get_all_variables() if not output_net.get_children(var)]
             
             # if there are no more nodes to prune
-            if not leaf_vars - set(queries + list(evidence.keys())):
+            if not set(leaf_vars) - set(Q + list(E.keys())):
                 break
             
             for leaf in leaf_vars:
-                if leaf not in queries and leaf not in evidence:
+                if leaf not in Q and leaf not in E:
                     output_net.del_var(leaf)
         
         return output_net
@@ -77,7 +75,7 @@ class BNReasoner:
                     graph.del_var(leaf)
         
         # remove outgoing edges for each node from Z
-        edges = graph.structure.out_edges(Z)
+        edges = deepcopy(graph.structure.out_edges(Z))
         for edge in edges:
             graph.del_edge(edge)
 
@@ -135,10 +133,31 @@ class BNReasoner:
         vars = set(itertools.chain.from_iterable(vars))
         vars.remove('p')
         vars = list(vars)
-        truth_table = pd.DataFrame(list(itertools.product([False, True], repeat=len(vars))), columns=vars)
+
+        # get all possible instantiations per var
+        vars_values = dict()
+        for factor in factors:
+            for col in factor:
+                if col == 'p':
+                    continue
+                if col not in vars_values:
+                    vars_values[col] = factor[col].unique().tolist()
+                else:
+                    vars_values[col] += factor[col].unique().tolist()
+        
+        # remove duplicates
+        for key, val in vars_values.items():
+            vars_values[key] = list(set(val))
+
+        # dict to list in order of columns
+        possible_values_ordered_by_column_name = []
+        for var in vars:
+            possible_values_ordered_by_column_name.append(vars_values[var])
+
+        # construct truth_table as cartesian product of all possible instantiations
+        truth_table = pd.DataFrame(list(itertools.product(*possible_values_ordered_by_column_name)), columns=vars)
         truth_table['p'] = 1.0
 
-        # update truth_table by a multiplication of p from compatible rows from factors
         for factor in factors:
             vars = set(factor.columns.values.tolist())
             vars.remove('p')
@@ -174,24 +193,21 @@ class BNReasoner:
                         score += 1
             min_fill_degrees.append((node_name, score))
 
-        print(f"min fill degrees {min_fill_degrees}")
         min_fill_degrees.sort(key=lambda x: x[1])
         ordered = [x[0] for x in min_fill_degrees]
             
         return ordered
 
-    def variable_elimination(self, X: list, ordering="min_degree") -> pd.DataFrame:
+    def variable_elimination(self, factors: list, X: list, ordering="min_degree") -> pd.DataFrame:
         if ordering == "min_degree":
             X = self.min_degree_ordering(X)
         else:
             X = self.min_fill_ordering(X)
-
-        all_cpts = self.bn.get_all_cpts().values()
         
         prev_factor = None
         for x in X:
-            x_cpts = [cpt for cpt in all_cpts if x in cpt.columns]
-            all_cpts = [cpt for cpt in all_cpts if x not in cpt.columns]
+            x_cpts = [cpt for cpt in factors if x in cpt.columns]
+            factors = [cpt for cpt in factors if x not in cpt.columns]
             # add prev_factor
             x_cpts += [prev_factor]
             multiplied = self.factors_multiplication(x_cpts)
@@ -201,3 +217,68 @@ class BNReasoner:
         # prev_factor after all iterations is a final marginalized factor
         return prev_factor
 
+    def marginal_distribution(self, Q: list, E: dict) -> pd.DataFrame:
+        all_cpts = self.bn.get_all_cpts()
+        # reduce factors due to evidence E
+        reduced_cpts = []
+        vars_to_eliminate = set()
+        for cpt in all_cpts.values():
+            # remove rows that are not compatible with E
+            evidence = pd.Series(E)
+            reduced_cpt = self.bn.get_compatible_instantiations_table(evidence, cpt)
+            reduced_cpts.append(reduced_cpt)
+            vars = [x for x in reduced_cpt.columns if x not in Q]
+            vars_to_eliminate.update(vars)
+       
+        # variable elimination
+        vars_to_eliminate.remove('p')
+        vars_to_eliminate = list(vars_to_eliminate)
+
+        posterior_marginal = self.variable_elimination(reduced_cpts, vars_to_eliminate)
+        
+        return posterior_marginal
+
+    # marginalize by maxing-out
+    def max_out(self, factor: pd.DataFrame, vars: list) -> tuple:
+        grouping_vars = [col_name for col_name in factor.columns if col_name not in ["p", " "] + vars]
+        # it is the same as summing out but instead of summing grouped rows we choose the one with max p
+        extended = factor.groupby(grouping_vars).max().reset_index()
+
+        marginalized = extended
+        for v in vars:
+            marginalized = marginalized.drop(v, axis=1)
+
+        return marginalized, extended
+
+    # Maximum A-Posteriori Query
+    # it also checks for MPE case
+    def MAP(self, Q: list, E: dict) -> dict:
+        all_variables = self.bn.get_all_variables()
+        MPE = Q == [var for var in all_variables if var not in E.keys()]
+
+        if MPE:
+            print("using mpe")
+            all_cpts = self.bn.get_all_cpts()
+            factor = self.factors_multiplication(list(all_cpts.values()))
+            # drop rows incompatible with instantiation
+            for key, val in E.items():
+                factor = factor.loc[factor[key] == val]
+            distribution = factor.reset_index()
+
+
+        else:
+            distribution = self.marginal_distribution(Q, E)
+
+        # return row with highest p
+        idx_max = distribution['p'].idxmax()
+        max = distribution.iloc[idx_max]
+        
+        p = 0
+        instantiations = dict()
+        for val in max.items():
+            if val[0] == 'p':
+                p = val[1]
+            else:
+                instantiations[val[0]] = val[1]
+
+        return p, instantiations
